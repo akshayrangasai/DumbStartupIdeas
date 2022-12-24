4c1f5a06c56df262711c4e8884b714d4cfe18932
akshayrangasai <akshayrangasai.d@gmail.com>
2022-12-24 01:56:48 -0500

    Added connection details, some more packages and scripts to run server. Good news is server runs now, but bad news is that's all it does. Need to set up auth and API tomorrow

--- Quantitative_Dating/server/index.js
+++ Quantitative_Dating/server/index.js
@@ -1,8 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+
+const mongo_user = process.env.DB_USER;
+const mongo_pwd = process.env.DB_PWD;
+const mongo_url = process.env.DB_URL;
+const _PORT = process.env.SERVER_PORT;
+
+const mongoConnString = "mongodb+srv://"+mongo_user+":"+mongo_pwd+"@"+mongo_url;
+
+mongoose.connect(mongoConnString);
+
+const mongoConnection = mongoose.connection;
+
+mongoConnection.on('error', console.error.bind(console, 'Console Error'));
+mongoConnection.once('open',() => console.log('Connected to DB'));
 
 const app = express();
-app.use(cors())
 
-app.listen(3000, () => console.log('Listening in port 3000'));
+app.use(cors({origin:process.env.CORS_ORIGIN}))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(_PORT, () => console.log('Listening in port', _PORT));

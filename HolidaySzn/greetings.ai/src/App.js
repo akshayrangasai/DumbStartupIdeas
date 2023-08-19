bf05fb0386ab0b0cdeb5660665326aa9db16b8b0
Akshay Rangasai <arangasai@AkshaysGIFMaker.lan>
2023-08-19 17:27:03 -0400

    Updated login to make it easy to set user and prevent login bug

--- HolidaySzn/greetings.ai/src/App.js
+++ HolidaySzn/greetings.ai/src/App.js
@@ -16,23 +16,12 @@ function App() {
 	const [name, setName] = useState(null);
 	const [canSendEmail, setcanSendEmail] = useState(false);
 	const [image, setImage] = useState(false);
+	const [inSession, setinSession] = useState(false);
 	
 	//const [hitUser, sethitUser] = useState(false);
 
   /* Check user exists, if not check with server and then add local storage, if not change stack */
 	const getUser = async () => {
-	
-		if(localStorage.getItem('user'))	
-		{
-		setUser(localStorage.getItem('user'))
-		setcanSendEmail(localStorage.getItem('canSendEmail'))
-		setName(localStorage.getItem('name'))
-		setImage(localStorage.getItem('image'))
-		
-		}
-		else
-		{
-
 		try {
 			const serverURL = process.env.REACT_APP_SERVER_URL; 
 			const url = serverURL.concat("/auth/user");
@@ -46,14 +35,14 @@ function App() {
 			localStorage.setItem("name", data.name)
 			localStorage.setItem("canSendEmail", data.canSendEmail)
 			localStorage.setItem("image", data.image)
-			
+			setinSession(true);	
 			}
-		catch(err)
-		{
-		//console.log(err);
-
-		}
-	}
+			catch(err)
+			{
+			localStorage.clear()
+			setinSession(false);
+			}
+	
 };
 
 	useEffect(() => {
@@ -64,7 +53,7 @@ function App() {
 		<div className="container">
 			<ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']} minBreakpoint="xxs" > 
 				
-				{user ? <AuthenticatedApp user = {user} canSendEmail = {canSendEmail} name = {name} image = {image}/> : <PublicLanding /> }
+				{inSession ? <AuthenticatedApp user = {user} canSendEmail = {canSendEmail} name = {name} image = {image}/> : <PublicLanding /> }
 			
 			</ThemeProvider>
 		</div>
